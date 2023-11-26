1b4aba9df970cf555c8231eb15b4871df649c474
Julio Jimenez <julio@julioj.com>
2023-11-26 17:25:55 -0500

    c
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ c-tutorial/hello.c
@@ -0,0 +1,7 @@
+// Hello world program
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");  // Actually do the work here
+}
---
+++ network-guide/talker.c
@@ -0,0 +1,21 @@
+// 
+// talker.c - A datagram "client" demo
+// 
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SERVERPORT "4950"   // the port users will be connecting to
+
+int main(int argc, char *argv[]) {
+    int sockfd;
+    
+}
