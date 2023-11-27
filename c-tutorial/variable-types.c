2c906fa6818d97e6a3e691eb5e10974c8be5ab55
Julio Jimenez <julio@julioj.com>
2023-11-27 00:04:31 -0500

    variable types
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ c-tutorial/variable-types.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 2;
+    float f = 3.14;
+    char *s = "Hello, World!";  // char * ("char pointer") is the string type
+
+    printf("%s  i = %d and f = %f!\n", s, i, f);
+}
