cd1236a40d4330abec93363a9b5ab232340f1432
Julio Jimenez <julio@julioj.com>
2023-11-27 22:50:13 -0500

    more
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- network-guide/pollserver.c
+++ network-guide/pollserver.c
@@ -72,5 +72,58 @@ int get_listener_socket(void) {
 
 // Add a new file descriptor to the set
 void add_to_pfds(struct pollfd *pfds[], int newfd, int *fd_count, int *fd_size) {
-    
+    // If we don't have room, add more space in the pfds array
+    if (*fd_count == *fd_size) {
+        *fd_size *= 2;  // Double it
+        *pfds = realloc(*pfds, sizeof(**pfds) * (*fd_size));
+    }
+    (*pfds)[*fd_count].fd = newfd;
+    (*pfds)[*fd_count].events = POLLIN;
+    (*fd_count)++;
+}
+
+// Remove an index from the set
+void del_from_pfds(struct pollfd pfds[], int i, int *fd_count) {
+    // Copy the one from the end over this one
+    pfds[i] = pfds[*fd_count - 1];
+    (*fd_count)--;
+}
+
+// Main
+int main(void) {
+    int listener;   // Listening socket descriptor
+
+    int newfd;      // Newly accept()ed socket descriptor
+    struct sockaddr_storage remoteaddr; // Client address
+    socklen_t addrlen;
+
+    char buf[256];  // Buffer for client data
+
+    char remoteIP[INET6_ADDRSTRLEN];
+
+    // Start off with room for 5 connections
+    // (We'll realloc as necessary)
+    int fd_count = 0;
+    int fd_size = 5;
+    struct pollfd *pfds = malloc(sizeof *pfds * fd_size);
+
+    // Set up and get a listening socket
+    listener = get_listener_socket();
+
+    if (listener == -1) {
+        fprintf(stderr, "error getting listening socket\n");
+        exit(1);
+    }
+
+    // Add the listener to set
+    pfds[0].fd = listener;
+    pfds[0].events = POLLIN;    // Report ready to read on incoming connection
+
+    fd_count = 1;   // For the listener
+
+    // Main loop
+    for (;;) {
+        
+    }
+
 }
