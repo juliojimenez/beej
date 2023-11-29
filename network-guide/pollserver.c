d7c5f1759baf5f3e470726fd904906f69aa929fa
Julio Jimenez <julio@julioj.com>
2023-11-28 23:06:42 -0500

    pollserver
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- network-guide/pollserver.c
+++ network-guide/pollserver.c
@@ -123,7 +123,60 @@ int main(void) {
 
     // Main loop
     for (;;) {
-        
-    }
+        int poll_count = poll(pfds, fd_count, -1);
 
+        if (poll_count == -1) {
+            perror("poll");
+            exit(1);
+        }
+
+        // Run through the existing connections looking for data to read
+        for (int i = 0; i < fd_count; i++) {
+            // Check if someone's ready to read
+            if (pfds[i].revents & POLLIN) { // We got one!!
+                if (pfds[i].fd == listener) {
+                    // If listener is ready to read, handle new connection
+                    addrlen = sizeof remoteaddr;
+                    newfd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen);
+
+                    if (newfd == -1) {
+                        perror("accept");
+                    } else {
+                        add_to_pfds(&pfds, newfd, &fd_count, &fd_size);
+                        printf("pollserver: new connection from %s on socket %d\n", inet_ntop(remoteaddr.ss_family, get_in_addr((struct sockaddr *)&remoteaddr), remoteIP, INET6_ADDRSTRLEN), newfd);
+                    }
+                } else {
+                    // If not the listener, we're just a regular client
+                    int nbytes = recv(pfds[i].fd, buf, sizeof buf, 0);
+                    int sender_fd = pfds[i].fd;
+
+                    if (nbytes <= 0) {
+                        // Got error or connection closed by client
+                        if (nbytes == 0) {
+                            // Connection closed
+                            printf("pollserver: socket %d hung up\n", sender_fd);
+                        } else {
+                            perror("recv");
+                        }
+                        close(pfds[i].fd);  // Bye!
+                        del_from_pfds(pfds, i, &fd_count);
+                    } else {
+                        // We got some good data from a client
+                        for (int j = 0; j < fd_count; j++) {
+                            // Send to everyone!
+                            int dest_fd = pfds[j].fd;
+                            
+                            // Except the listener and ourselves
+                            if (dest_fd != listener && dest_fd != sender_fd) {
+                                if (send(dest_fd, buf, nbytes, 0) == -1) {
+                                    perror("send");
+                                }
+                            } 
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 0;
 }
