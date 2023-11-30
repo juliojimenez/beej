5295f011267c5e4b10b093636bb977e8cdaf9ab2
Julio Jimenez <julio@julioj.com>
2023-11-29 23:44:11 -0500

    select
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- network-guide/select.c
+++ network-guide/select.c
@@ -1,4 +1,14 @@
 // 
 // select.c - A select() demo
-// 
+//
 
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define STDIN 0	// file descriptor for standard input
+
+int main (void) {
+
+}
