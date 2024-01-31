fe69b05e53704c8696dea118def4cf213e429dbd
Julio Jimenez <julio@julioj.com>
2024-01-31 16:48:20 -0500

    ugh
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- bnet/bnet.c
+++ bnet/bnet.c
@@ -1,6 +1,6 @@
-// 
+//
 // bnet.c - Utility socket library
-// 
+//
 
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -10,43 +10,49 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-int showIp(char *hostname) {
-    struct addrinfo hints, *res, *p;
-    int status;
-    char ipstr[INET6_ADDRSTRLEN];
+struct show_ip {
+  char *ipver;
+  char *ip;
+};
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;    // AF_INET or AF_INET6 to force version
-    hints.ai_socktype = SOCK_STREAM;
+struct show_ip *showIp(char *hostname) {
+  struct addrinfo hints, *res, *p;
+  int status;
+  char ipstr[INET6_ADDRSTRLEN];
 
-    if ((status = getaddrinfo(hostname, NULL, &hints, &res)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        return 2;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((status = getaddrinfo(hostname, NULL, &hints, &res)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    return 2;
+  }
+
+  printf("IP addresses for %s:\n\n", hostname);
+
+  for (p = res; p != NULL; p = p->ai_next) {
+    void *addr;
+    char *ipver;
+
+    // get the pointer to the address itself,
+    // different fields in IPv4 and IPv6:
+    if (p->ai_family == AF_INET) { // IPv4
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+      addr = &(ipv4->sin_addr);
+      ipver = "IPv4";
+    } else {
+      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+      addr = &(ipv6->sin6_addr);
+      ipver = "IPv6";
     }
 
-    printf("IP addresses for %s:\n\n", hostname);
-    
-    for (p = res; p != NULL; p = p->ai_next) {
-        void *addr;
-        char *ipver;
-
-        // get the pointer to the address itself,
-        // different fields in IPv4 and IPv6:
-        if (p->ai_family == AF_INET) {  // IPv4
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            addr = &(ipv4->sin_addr);
-            ipver = "IPv4";
-        } else {
-            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-            addr = &(ipv6->sin6_addr);
-            ipver = "IPv6";
-        }
-
-        // convert the IP to a string and print it:
-        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-        printf("  %s: %s\n", ipver, ipstr);
-    }
-    freeaddrinfo(res);  // free the linked list
-    return 0;
+    // convert the IP to a string and print it:
+    inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+    struct show_ip result_ip;
+    result_ip.ipver = ipver;
+    result_ip.ip = ipstr;
+  }
+  freeaddrinfo(res); // free the linked list
+  return 0;
 }
-
